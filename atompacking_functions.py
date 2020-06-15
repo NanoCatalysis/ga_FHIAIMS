ed5010b7991c9c6505c6eba95bd09febab641901
raet-94 <raet94@gmail.com>
2020-06-15 12:57:29 -0500

    Fitness prob functions

--- atompacking_functions.py
+++ atompacking_functions.py
@@ -488,4 +488,36 @@ def create_pool(N= 55, atom = "Au", path = "", R_min = 2.0, Num_decimals =4, Dis
 			print("Run number  '{}' converged".format(x)) 	
 
 
+def  probability (E_i, E_min, E_max):
+	p_i = (E_i - E_min )/(E_max - E_ min)
+	return p_i 
+
+def f_exp(alpha, p_i):
+	f_i =math.pow(math.e, -alpha* p_i)	
+	return f_i
+
+def f_tanh( p_i):
+	f_i = (1/2)*(1-math.tanh(2*p_i -1))	
+	return f_i
+
+def f_lin(p_i):
+	f_i = 1- 0.7*p_i
+	return f_i 
+
+
+
+def kick( athom):
+	r_min=-1
+	r_max=1
+	x_i = athom[0]
+	y_i = athom[1]
+	z_i= athom[2]
+    theta = random.uniform(0, 2 * math.pi)
+    phi = random.uniform(0, 2 * math.pi)
+    r = random.uniform(r_min,r_max)
+    x = (r * math.cos(theta) * math.sin(phi)) +x_i
+    y = (r * math.sin(theta) * math.sin(phi)) + y_i
+    z = (r * math.cos(phi)) + z_i
+    vector = [x,y,z]
+    return  vector
 
