c9e4845f0d5eca0107fa0788b4f0a1121d9717ac
Rodrigo Espinola <raet94@gmail.com>
2020-08-30 14:22:11 -0500

    30/08/2020 2:20PM

--- correr_pool_au6.py
+++ correr_pool_au6.py
@@ -1,4 +1,5 @@
-import atompacking_functions as af
+#import atompacking_functions as af
+#af.create_pool(6, "Au","pools_au6/")
 
-
-af.create_pool(6, "Au","pools_au6/")
+import init_ga as init 
+init.init_calc(6, "Au", "pools_au6/",16,"g1")
