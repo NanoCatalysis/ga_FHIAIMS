0cf24dc0f3857d59acec6caae1c99dbd1b272426
raet-94 <raet94@gmail.com>
2020-09-02 16:38:37 -0500

    2/9/2020 4:38PM

--- atompacking_functions.py
+++ atompacking_functions.py
@@ -560,7 +560,8 @@ def create_pool(N= 55, atom = "Au", path = "", R_min = 2.0, Num_decimals =4, Dis
 	print(directories)
 	for x in directories:
 		try:
-			run_raw = "./" +x +"/shforrunning.sh"
+			run_raw =  +x +"/shforrunning.sh"
+
 			subprocess.call(run_raw,universal_newlines = True, shell = True)
 		except :
 			print("Error running")
