4b5f9be002ae05f20405e163c45f016448f904de
raet-94 <raet94@gmail.com>
2020-09-23 18:00:01 -0500

    new run7

--- run_Au6.py
+++ run_Au6.py
@@ -41,7 +41,7 @@ for x in directories:
 	dir_list = os.listdir(path_dum)  
 	print("Files and directories in '", path_dum, "' :", dir_list)  
 	try:
-		run_raw = "./" +path_dum +"/shforrunning.sh"
+		run_raw = "." +path_dum +"/shforrunning.sh"
 		subprocess.call(run_raw,universal_newlines = True, shell = True)
 	except :
 		print("Error running")
