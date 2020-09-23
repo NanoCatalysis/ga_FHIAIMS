8dd3736c577cb5e50e2b4a625583bfc46480d9f0
raet-94 <raet94@gmail.com>
2020-09-23 18:07:07 -0500

    new run8

--- run_Au6.py
+++ run_Au6.py
@@ -39,9 +39,10 @@ time.sleep(20)
 for x in directories:
 	path_dum = os.path.join(THIS_FOLDER, x)
 	dir_list = os.listdir(path_dum)  
-	print("Files and directories in '", path_dum, "' :", dir_list)  
-	try:
-		run_raw = "." +path_dum +"/shforrunning.sh"
-		subprocess.call(run_raw,universal_newlines = True, shell = True)
-	except :
-		print("Error running")
+	print("Files and directories in '", path_dum, "' :", dir_list)
+	if os.path.exists(path_dum +'/shforrunning.sh') == True: 
+		try:
+			run_raw = "." +path_dum +"/shforrunning.sh"
+			subprocess.call(run_raw,universal_newlines = True, shell = True)
+		except :
+			print("Error running")
