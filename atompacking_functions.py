4ca8d50e1740cfa4dec90a89fbd098f445e43972
raet-94 <raet94@gmail.com>
2020-09-24 11:40:57 -0500

    24/9/2020 11:38 AM

--- atompacking_functions.py
+++ atompacking_functions.py
@@ -277,12 +277,12 @@ def create_directory(size =55, atom ="Au", path ="", add =0):
 	count = add
 	original_size = size
 	original_atom = atom 
-	original_path = path+"/" 
+	original_path = path 
 	directory_name =original_atom + str(original_size)
-	directory_path = original_path+ directory_name	
+	directory_path = original_path+"/"+ directory_name	
 	answer = "not changing answer"
 	if count != 0:
-		 directory_path = original_path + directory_name + "_" + str(count)
+		 directory_path = original_path+ "/" + directory_name + "_" + str(count)
 		 answer = directory_path
 	if not os.path.exists(directory_path):
 		os.mkdir(directory_path)
