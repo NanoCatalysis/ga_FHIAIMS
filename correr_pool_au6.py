ae3df1030f51e73b63882c54298cba80910c5730
raet-94 <raet94@gmail.com>
2020-11-10 11:41:04 -0600

    10/11/20 1141

--- correr_pool_au6.py
+++ correr_pool_au6.py
@@ -5,4 +5,4 @@ import atompacking_functions as af
 #af.init_calc(6, "Au", "pools_au6/",6,"g1")
 
 file_dirs= af.create_all_files(Size= 6, Atom="Au", Path="pools_au6",Cores= 16,Node= "g1")
-af.check_convergence_pool( file_dirs =file_dirs, Atom = "Au", Size = 6, path = "pools_au6")
+#af.check_convergence_pool( file_dirs =file_dirs, Atom = "Au", Size = 6, path = "pools_au6")
