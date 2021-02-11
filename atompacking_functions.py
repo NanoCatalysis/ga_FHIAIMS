894d19b911ff3e3cfbd006db300a929cd87dcb6d
Rodrigo Espinola <raet94@gamil.com>
2021-02-11 12:14:41 -0600

    solving issue in energies 5

--- atompacking_functions.py
+++ atompacking_functions.py
@@ -1047,11 +1047,11 @@ def read_data(filename="",path="./"):
         [print(x) for x in vector_line ]
         print(vector_line)
         if vector_line != [''] and lines.index(line) > 1:
-            Energies_1.append(float(vector_line[1]))
-            Normalized_energies_1.append(float(vector_line[2]))
-            fitnessed_energies_1.append(float(vector_line[3]))
-            probabilities_1.append(float(vector_line[4]))
-            directories_1.append(vector_line[5])
+            Energies_1.append(float(vector_line[0]))
+            Normalized_energies_1.append(float(vector_line[1]))
+            fitnessed_energies_1.append(float(vector_line[2]))
+            probabilities_1.append(float(vector_line[3]))
+            directories_1.append(vector_line[4])
     return Energies_1, Normalized_energies_1, fitnessed_energies_1, probabilities_1, directories_1, step, Number_ofGenerations
 
 def Cicle_mutation(data_last_step= "", path = "", name="", cores =16, file_energies="", Atom ="Au", Size=52):
